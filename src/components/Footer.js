2db729cc1ffc811649d12e71444567b2f5cc7a73
fullstack developer <161649636+stacksculptor@users.noreply.github.com>
2025-08-22 08:05:58 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,7 +38,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-bottom">
-          <p>&copy; 2025 K VENUS LLC. All rights reserved. | EIN: 39-2819775</p>
+          <p>&copy; 2025 K VENUS LLC. All rights reserved.</p>
         </div>
       </div>
     </footer>
